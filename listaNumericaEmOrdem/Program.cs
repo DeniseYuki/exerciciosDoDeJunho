006785069e04a3656b38bd7aacc7ad82e7ec185c
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2023-06-12 20:32:53 -0300

    teste

---
+++ listaNumericaEmOrdem/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program {
+
+    static void Main()
+    {
+        List<int> numero = new List<int> { 15, 25, 36, 99, 2, 3, 55 };
+
+        numero.Sort();
+
+        Console.WriteLine("Numeros em ordem");
+        foreach (int ordemNumerica in numero)
+
+        {
+            Console.WriteLine(ordemNumerica);
+            
+        }
+
+    }
+}
